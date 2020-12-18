28d5ac01f675b1f49e0bcc46ae56d20b16c4fe9f
root <root@8b2bc97c337c>
2020-12-18 01:27:57 +0000

    KRIMASS

--- src/_01_random/_5_magic_8_ball/Magic8Ball.java
+++ src/_01_random/_5_magic_8_ball/Magic8Ball.java
@@ -6,10 +6,42 @@ package _01_random._5_magic_8_ball;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class Magic8Ball {
 
 	// 1. Make a main method that includes all the steps below
-
+	public static void main(String[] args) {
+	 JOptionPane.showInputDialog("Ask a Question");	
+	
+	Random randomMaker = new Random ();
+	
+	
+	int randomNumber = randomMaker.nextInt(4);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
 	// 2. Get the user to enter a question for the 8 ball to answer
 	
 

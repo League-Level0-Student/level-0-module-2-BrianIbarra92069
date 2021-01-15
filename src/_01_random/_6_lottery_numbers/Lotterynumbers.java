7177543339838617e33f5e954cdbebf182f52722
student <student@ip-172-31-62-34.us-west-2.compute.internal>
2021-01-14 17:27:37 -0800

    HI

---
+++ src/_01_random/_6_lottery_numbers/Lotterynumbers.java
@@ -0,0 +1,25 @@
+package _01_random._6_lottery_numbers;
+
+import java.util.Random;
+
+public class Lotterynumbers {
+
+public static void main(String[] args) {
+	
+	Random randomNumber = new Random ();
+	
+	
+	int randomSystem = randomNumber.nextInt(7);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+} 
+}

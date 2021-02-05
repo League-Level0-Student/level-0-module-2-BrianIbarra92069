a57dc725218d59000e1d05cc7ee7a4f5996b1028
student <student@ip-172-31-52-101.us-west-2.compute.internal>
2021-02-04 17:31:52 -0800

    Kay

--- src/_01_random/_6_lottery_numbers/Lotterynumbers.java
+++ src/_01_random/_6_lottery_numbers/Lotterynumbers.java
@@ -2,6 +2,8 @@ package _01_random._6_lottery_numbers;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class Lotterynumbers {
 
 public static void main(String[] args) {
@@ -12,10 +14,11 @@ public static void main(String[] args) {
 	int randomSystem = randomNumber.nextInt(7);
 	int randomMessage = randomNumber.nextInt(7);
 	int randomMail = randomNumber.nextInt(7);
-	int random
-	
-	
-	
+	int randomSound = randomNumber.nextInt(7);
+	int randomClass = randomNumber.nextInt(7);
+	int randomPictures = randomNumber.nextInt(7);
+	String Message = randomSystem+""+randomMessage+""+randomMail+""+randomSound+""+randomClass+""+randomPictures;
+	JOptionPane.showMessageDialog(null, Message);
 	
 	
 	

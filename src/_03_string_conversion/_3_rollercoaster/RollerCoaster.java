69633fe01ba0fd6c49b7d7fb1e7e81e90dfca3a6
student <student@ip-172-31-53-36.us-west-2.compute.internal>
2021-05-20 17:27:50 -0700

    Dream

--- src/_03_string_conversion/_3_rollercoaster/RollerCoaster.java
+++ src/_03_string_conversion/_3_rollercoaster/RollerCoaster.java
@@ -4,38 +4,37 @@ import javax.swing.JOptionPane;
 
 public class RollerCoaster {
 
-	// 1) Make a main method that includes all the steps below…. 
-		public static void main(String[] args) {
-			
+	// 1) Make a main method that includes all the steps below….
+	public static void main(String[] args) {
+
 		String heightToRide = JOptionPane.showInputDialog("Please enter you Height");
-		
+
 		int heightAsInt = Integer.parseInt(heightToRide);
-		// 2) Ask the user to enter their height in inches using JOptionPane and set it to the variable heightToRide
-		
-		
-			if( heightAsInt > 42 ) { }
-			if( heightToRide > 42) { }
-		// Do you see the heightToRide > 42 underlined red? That is because you are comparing a STRING to an INTEGER.
-		
-		// 3) Convert heightToRide to heightAsInt using Integer.parseInt();  Hint: int heightAsInt = Integer.parse... 
-	
+		// 2) Ask the user to enter their height in inches using JOptionPane and set it
+		// to the variable heightToRide
+
+		if (heightAsInt > 42) {
+			JOptionPane.showMessageDialog(null, "You may ride the rollercoster");
+		}
+		if (heightAsInt <= 42) {
+			JOptionPane.showMessageDialog(null, "Come Back Later when You're taller");
+		}
+
+		// Do you see the heightToRide > 42 underlined red? That is because you are
+		// comparing a STRING to an INTEGER.
+
+		// 3) Convert heightToRide to heightAsInt using Integer.parseInt(); Hint: int
+		// heightAsInt = Integer.parse...
+
 		// 4) If the heightAsInt is greater than or equal to 48 inches tall print:
-	    // "Hurray! You are tall enough to ride the coaster alone!" Hint: use JoptionPane
-		
+		// "Hurray! You are tall enough to ride the coaster alone!" Hint: use
+		// JoptionPane
+
 		// 5) Else if the heightAsInt is greater than or equal to 42 inches tall print:
-		// "You can ride the coaster with someone else!" 
-	
-		// 6) else print: "You must be at least 42 inches tall to ride the roller coaster pal!"
-}
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		// "You can ride the coaster with someone else!"
+
+		// 6) else print: "You must be at least 42 inches tall to ride the roller
+		// coaster pal!"
+	}
+
 }

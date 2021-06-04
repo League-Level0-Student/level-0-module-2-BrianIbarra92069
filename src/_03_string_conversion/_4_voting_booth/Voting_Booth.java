1bbb2d3e63aadcbd9f24f4af7d357427d8d71b24
student <student@ip-172-31-62-198.us-west-2.compute.internal>
2021-06-03 17:26:27 -0700

    Tommyinnit

---
+++ src/_03_string_conversion/_4_voting_booth/Voting_Booth.java
@@ -0,0 +1,27 @@
+package _03_string_conversion._4_voting_booth;
+
+import javax.swing.JOptionPane;
+
+public class Voting_Booth {
+
+public static void main(String[] args) {
+
+	
+	String ageToVote = JOptionPane.showInputDialog("How Old are you?");
+	
+	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}

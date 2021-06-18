302d0ed3c86d99b497b9f184ea61b04e0a64b586
student <student@ip-172-31-49-110.us-west-2.compute.internal>
2021-06-17 17:28:41 -0700

    bloop

--- src/_03_string_conversion/_4_voting_booth/Voting_Booth.java
+++ src/_03_string_conversion/_4_voting_booth/Voting_Booth.java
@@ -7,7 +7,22 @@ public class Voting_Booth {
 public static void main(String[] args) {
 
 	
+	
 	String ageToVote = JOptionPane.showInputDialog("How Old are you?");
+	
+	
+	int voteAsInt = Integer.parseInt(ageToVote);
+	
+	if (voteAsInt > 16) {
+		JOptionPane.showMessageDialog(null, "You may be able to vote");
+	}
+	if (voteAsInt <= 16) {
+		JOptionPane.showMessageDialog(null, "Come back when your older");
+	}
+	
+	
+	
+	
 	
 	
 }
